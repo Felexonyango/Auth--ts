f248647f76506560d37947cf811fe70c045731b4
Felexonyango <felexonyango19@gmail.com>
2022-04-04 20:22:16 +0300

    fix

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,7 @@
 import mongoose, { ConnectOptions} from 'mongoose';
 import {MONGO_URI} from './confg'
 const connectDB = async () => {
-    // const MONGO_URI= "mongodb+srv://mern:mern@cluster0.prv2x.mongodb.net/myFirstDatabase?retryWrites=true"
+    
     try {
         await mongoose.connect(  MONGO_URI as string, {
             useNewUrlParser: true,
